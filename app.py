8f9179dd7fea74e35af55cc28257c3daa6a617cb
Alexander Neumann <aleneum@gmail.com>
2023-10-27 09:18:28 +0200

    simplify code for demo

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from utils import df_from_doc, faiss_search, create_search_index, wrap_print, ge
 import pathlib
 from tempfile import NamedTemporaryFile
 
-st.title("FAISSAL")
-st.subheader("Document QA Chatbot using LLaMA 2, FAISS, and LangChain")
-st.text("by @msuatgunerli")
+st.title("Entlassbriefanalyse")
+st.subheader("mit LLama 2, FAISS und LangChain")
 
 st.divider()
 
-uploaded_file = st.file_uploader("Upload a Single Document:", accept_multiple_files=False, type=['pdf', 'txt'])
+uploaded_file = st.file_uploader("Ein Dokument hochladen:", accept_multiple_files=False, type=['pdf', 'txt'])
 
 if uploaded_file is not None:
     filetype = pathlib.Path(uploaded_file.name).suffix
@@ -17,28 +16,34 @@ if uploaded_file is not None:
         f.write(uploaded_file.getbuffer())
         f.flush()
         docs = df_from_doc.df_from_doc(f.name, str(filetype).replace(".", ""))
+
+        with st.expander("Inhalt", expanded=True):
+            f.seek(0)
+            st.text(''.join(l.decode("utf-8") for l in f.readlines()))
+
     
-    model_name = st.selectbox("Select Sentence-Transformers Model for Embeddings:", ["all-MiniLM-L6-v2", "multi-qa-mpnet-base-dot-v1"])
+    # model_name = st.selectbox("Sentence-Transformers Model for Embeddings:", ["all-MiniLM-L6-v2", "multi-qa-mpnet-base-dot-v1"])
+    model_name = "all-MiniLM-L6-v2"
 
     pkl = create_search_index.create_search_index(docs, model_name)
-
-    question = st.text_input("Ask a Question:")
-
-    context = generate_context.generate_context(pkl, question, model_name, num_results = 5)
-
-    st.write("Estimated Context Length:", round(4/3*len(context.split())), "tokens", "\n")
-    wrap_print.wrap_print(context)
-
-    model_type = st.selectbox("Select LLM Type:", ["LLaMA-7B", "LLaMA-13B"])
+    # model_type = st.selectbox("Select LLM Type:", ["LLaMA-7B", "LLaMA-13B"])
+    model_type = "LLaMA-7B"
     llm = load_llm.load_llm(model_type=model_type, model_path=f"./models/{model_type}.gguf")
-    context_dependency = st.selectbox("Select Context Dependence Level (set to low if the model is failing to generate context dependent answers):", ["low", "medium", "high"])
+    
+    with st.form('user_form', clear_on_submit = False):
+        question = st.text_input("Stellen Sie Ihre Frage: ", value = "")
+        submit_button = st.form_submit_button(label="Frage stellen")
+        context = generate_context.generate_context(pkl, question, model_name, num_results = 5)
+        # st.write("Geschätzte Antwortlänge:", round(4/3*len(context.split())), "tokens", "\n")
+        wrap_print.wrap_print(context)
 
-    if st.button("Generate Response"):
-        st.write("Running, may take up to 60 seconds...")
-        with st.spinner(f"Running {model_type}..."):
-            output = run_llm.run_llm(llm, question, context, context_dependency)
+    # context_dependency = st.selectbox("Select Context Dependence Level (set to low if the model is failing to generate context dependent answers):",
+    #                                   ["low", "medium", "high"])
 
-        if output["choices"][0]["text"].split("###")[4][-1] != ".":
-            st.success(output["choices"][0]["text"].split("###")[4] + "...")
-        elif output["choices"][0]["text"].split("###")[4][-1] == ".":
-            st.success(output["choices"][0]["text"].split("###")[4])
+    if submit_button:
+        with st.spinner(f"{model_type} generiert Antwort..."):
+            output = run_llm.run_llm(llm, question, context)
+            print(output)
+            # answer = output["choices"][0]["text"].strip().replace("\"", "").split("\n")[0].split(r"([^Dr].")[0]
+            answer = output["choices"][0]["text"].replace("\"", "").split("Ich")[0]
+            st.success(f"Antwort: {answer}")
--- utils/run_llm.py
+++ utils/run_llm.py
@@ -1,13 +1,10 @@
-def run_llm(llm, question, context, context_dependency):
-    if context_dependency == "low":
-        output = llm(f"### Instruction: Answer the question based on the provided context. ### Question: {question} ### Context: {context} ### Answer:",
-            temperature= 0.7, top_k = 10000, repeat_penalty = 1.1, max_tokens = 2048, echo=True) # for 7B
-    elif context_dependency == "high":
-        output = llm(f"### Instruction: Answer the question based only on the provided context, if the information is not provided in the context you must answer the question by saying that the required information cannot be found in the context. ### Question: {question} ### Context: {context} ### Answer:",
-                     temperature= 0.3, top_k = 10000, repeat_penalty = 1.1, max_tokens = 2048, echo=True) # for 13B
-    elif context_dependency == "medium":
-        output = llm(f"### Instruction: Answer the question based only on the provided context, you are allowed to use external information as a last resort, if and only if the the required information cannot be found in the context. ### Question: {question} ### Context: {context} ### Answer:",
-                     temperature= 0.5, top_k = 10000, repeat_penalty = 1.1, max_tokens = 2048, echo=True) # experimental prompt
-    else:
-        raise ValueError("Invalid prompt style: {}".format(context_dependency))
+def run_llm(llm, question, context):
+    output = llm(f""""Verwende die folgenden Informationen, um die Frage des Benutzers zu beantworten.
+Wenn du die Antwort nicht weißt, sag einfach, dass du es nicht weißt, versuche nicht, eine Antwort zu erfinden.
+
+Kontext: {context}
+Frage: {question}
+
+Gib nur die hilfreichen Informationen aus dem Kontext wortwörtlich wieder und nichts anderes.
+Hilfreiche Antwort: """, temperature= 0.01, top_k = 10000, repeat_penalty = 1.1, max_tokens = 200, echo=False)
     return output
